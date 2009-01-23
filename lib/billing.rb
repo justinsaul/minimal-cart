a6b1ba270553e33af43c1f174e2f4d9924f39658
Ben Greenberg <beg4@case.edu>
2009-01-23 16:37:16 -0500

    cleaning up validations and adding initializer

--- lib/billing.rb
+++ lib/billing.rb
@@ -24,27 +24,27 @@ class Billing
   attr_accessor :zip
   attr_accessor :country
   attr_accessor :customer
+  attr_accessor :phone
 
-  def initialize
+  def initialize(attributes=nil)
     @errors = ActiveRecord::Errors.new(self)
     @customer = Customer.new
-  end
 
-  validates_presence_of :card_number
-  validates_presence_of :cvn
-  validates_presence_of :expiration_month
-  validates_presence_of :expiration_year
-  validates_presence_of :name
-  validates_presence_of :address1
-  validates_presence_of :city
-  validates_presence_of :state
-  validates_presence_of :zip
-
-  def validate
-    begin
-      the_country = ActiveMerchant::Country.find(country)
-    rescue
-      errors.add_to_base("Sorry, we cannot bill to #{country}")
+    if attributes
+      attributes.each do |key, val|
+        instance_variable_set(:"@#{key}", val)
+      end
     end
   end
+
+  validates_presence_of :card_number, :message => 'You must enter a Credit Card Number'
+  validates_presence_of :cvn, :message => 'You must enter your Credit Card Security Code'
+  validates_presence_of :expiration_month, :message => 'You must enter your Credit Card expiration month'
+  validates_presence_of :expiration_year, :message => 'You must enter your Credit Card expiration year'
+  validates_presence_of :name, :message => 'You must enter a billing name'
+  validates_presence_of :address1, :message => 'You must enter a billing address'
+  validates_presence_of :city, :message => 'You must enter a billing city'
+  validates_presence_of :state, :message => 'You must enter a billing state or province'
+  validates_presence_of :zip, :message => 'You must enter a billing ZIP or Postal code'
+  validates_presence_of :phone, :message => 'You must enter a billing phone number.'
 end
